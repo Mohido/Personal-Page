d4826a37daddfe9af70edfc2cba6cf0ea68fb002
Mohammed Al-Mahdawi <m.ghaithalmahdawi@gmail.com>
2024-06-25 20:13:14 +0200

    Finished the Main Responsiveness Feature

--- qwik-app/src/components/left-menu/index.tsx
+++ qwik-app/src/components/left-menu/index.tsx
@@ -1,4 +1,8 @@
-import { type QRL, component$, useStyles$ } from "@builder.io/qwik";
+/**
+ * TO MY FUTURE SELF, I just managed to make this work and responsive, please don't fok it up in the future!
+ */
+
+import { type QRL, component$, useStyles$, useSignal, useVisibleTask$, $, useOnWindow } from "@builder.io/qwik";
 import IndexCSS from "./index.css?inline";
 import { useLocation } from "@builder.io/qwik-city";
 
@@ -13,21 +17,53 @@ export interface LeftMenuProps {
 export const LeftMenu = component$((props: LeftMenuProps) => {
     useStyles$(IndexCSS);
     const loc = useLocation();
+    const hidclas = useSignal<string>("hidden-menu");
+    const refLM = useSignal<HTMLDivElement>();
+
+    // eslint-disable-next-line qwik/no-use-visible-task
+    useVisibleTask$(() => {
+        if(refLM.value?.clientHeight === 0){
+            hidclas.value = "hidden-menu";
+        }else{
+            hidclas.value = "shown-menu";
+        }
+    },{ strategy: 'document-ready' });
+
+    useOnWindow("resize", $((ev)=>{
+        if(hidclas.value === "hidden-menu" && (ev.target as Window).innerWidth > 720)
+            hidclas.value = "shown-menu";
+        else if(hidclas.value === "shown-menu" && (ev.target as Window).innerWidth < 720)
+            hidclas.value = "hidden-menu";
+    }));
+
+    const toggleMenu$ = $(()=>{
+        if(refLM.value?.clientHeight === 0){
+            hidclas.value = "shown-menu";
+        }else{
+            hidclas.value = "hidden-menu";
+        }
+    });
+
     return (
-        <div class="left-menu">
-            <div class="profile-image" style={`background-image: url(${loc.url.origin}/profile_picture.jpg)`}></div>
-            <h1 class="header">Mohido</h1>
-            <ul>
-                {props.items.map((item) => (
-                    <li class="menu-item" key={item} onClick$={() => props.onClick$(item)}>
-                        {item}
-                    </li>
-                ))}
-            </ul>
-            <div class="footer">
-                {/* Copyright message */}
-                <p>&copy; 2024 Mohammed Al-Mahdawi. All rights reserved.</p>
+        <>
+            <div class="menu-toggle-button" onClick$={toggleMenu$}>☰</div>
+            <div ref={refLM} class={"left-menu " + hidclas.value}>
+                <div class="profile-image" style={`background-image: url(${loc.url.origin}/profile_picture.jpg)`}></div>
+                <h1 class="header">Mohido</h1>
+                <ul>
+                    {props.items.map((item) => (
+                        <li class="menu-item" key={item} onClick$={() => props.onClick$(item)}>
+                            {item}
+                        </li>
+                    ))}
+                </ul>
+                <div class="footer">
+                    {/* Copyright message */}
+                    <p>&copy; 2024 Mohammed Al-Mahdawi. All rights reserved.</p>
+                </div>
             </div>
-        </div>
+        
+        </>
+        
     );
 });
